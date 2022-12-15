0ae5d66bd403bde50b82ab29af6e812b7f0e4b49
Mark Altmann <mark.altmann@satellytes.com>
2022-12-15 08:52:11 +0100

    chore: format

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 const core = require("@actions/core");
 const process = require("process");
 const HttpClient = require("@actions/http-client").HttpClient;
-const { createLogger, format, transports } = require("winston");
+const {createLogger, format} = require("winston");
 const LokiTransport = require("winston-loki");
 const githubAPIUrl = "https://api.github.com";
-const { printf } = format;
+const {printf} = format;
 const gh_log_regex =
-  /^\s?(?<timestamp>((19|20)[0-9][0-9])[-](0[1-9]|1[012])[-](0[1-9]|[12][0-9]|3[01])[T]([01][1-9]|[2][0-3])[:]([0-5][0-9])[:]([0-5][0-9])[.](?<nanosec>[0-9][0-9][0-9][0-9][0-9][0-9][0-9])[Z])\s(?<log>.*){0,1}/;
+    /^\s?(?<timestamp>((19|20)[0-9][0-9])[-](0[1-9]|1[012])[-](0[1-9]|[12][0-9]|3[01])[T]([01][1-9]|[2][0-3])[:]([0-5][0-9])[:]([0-5][0-9])[.](?<nanosec>[0-9][0-9][0-9][0-9][0-9][0-9][0-9])[Z])\s(?<log>.*){0,1}/;
 
 /**
  *
@@ -14,12 +14,12 @@ const gh_log_regex =
  * @returns
  */
 export function getClient(ghToken) {
-  return new HttpClient("gh-http-client", [], {
-    headers: {
-      Authorization: `token ${ghToken}`,
-      "Content-Type": "application/json",
-    },
-  });
+    return new HttpClient("gh-http-client", [], {
+        headers: {
+            Authorization: `token ${ghToken}`,
+            "Content-Type": "application/json",
+        },
+    });
 }
 
 /**
@@ -31,29 +31,29 @@ export function getClient(ghToken) {
  * @returns
  */
 export async function fetchJobs(httpClient, repo, runId, allowList) {
-  const url = `${githubAPIUrl}/repos/${repo}/actions/runs/${runId}/jobs`;
-  const res = await httpClient.get(url);
+    const url = `${githubAPIUrl}/repos/${repo}/actions/runs/${runId}/jobs`;
+    const res = await httpClient.get(url);
 
-  if (res.message.statusCode === undefined || res.message.statusCode >= 400) {
-    throw new Error(`HTTP request failed: ${res.message.statusMessage}`);
-  }
-
-  const body = await res.readBody();
-  const jobs = [];
-  const all = allowList.length === 0;
-  for (const j of JSON.parse(body).jobs) {
-    // if there's an allow list, skip job accordingly
-    if (!all && !allowList.includes(j.name)) {
-      continue;
+    if (res.message.statusCode === undefined || res.message.statusCode >= 400) {
+        throw new Error(`HTTP request failed: ${res.message.statusMessage}`);
     }
 
-    jobs.push({
-      id: j.id,
-      name: j.name,
-    });
-  }
+    const body = await res.readBody();
+    const jobs = [];
+    const all = allowList.length === 0;
+    for (const j of JSON.parse(body).jobs) {
+        // if there's an allow list, skip job accordingly
+        if (!all && !allowList.includes(j.name)) {
+            continue;
+        }
 
-  return jobs;
+        jobs.push({
+            id: j.id,
+            name: j.name,
+        });
+    }
+
+    return jobs;
 }
 
 /**
@@ -65,15 +65,15 @@ export async function fetchJobs(httpClient, repo, runId, allowList) {
  */
 
 export async function fetchLogs(httpClient, repo, job) {
-  const url = `${githubAPIUrl}/repos/${repo}/actions/jobs/${job.id}/logs`;
-  const res = await httpClient.get(url);
+    const url = `${githubAPIUrl}/repos/${repo}/actions/jobs/${job.id}/logs`;
+    const res = await httpClient.get(url);
 
-  if (res.message.statusCode === undefined || res.message.statusCode >= 400) {
-    throw new Error(`HTTP request failed: ${res.message.statusMessage}`);
-  }
+    if (res.message.statusCode === undefined || res.message.statusCode >= 400) {
+        throw new Error(`HTTP request failed: ${res.message.statusMessage}`);
+    }
 
-  const body = await res.readBody();
-  return body.split("\n");
+    const body = await res.readBody();
+    return body.split("\n");
 }
 
 /**
@@ -82,118 +82,118 @@ export async function fetchLogs(httpClient, repo, job) {
  * @returns
  */
 export function getCommaSeparatedInput(value) {
-  let val = [];
-  if (value !== "") {
-    val = value.split(",");
-    val = val.map((s) => s.trim());
-  }
+    let val = [];
+    if (value !== "") {
+        val = value.split(",");
+        val = val.map((s) => s.trim());
+    }
 
-  return val;
+    return val;
 }
 
 export async function run() {
-  try {
-    // retrieve config params
+    try {
+        // retrieve config params
 
-    // Github repo token
-    const repoToken = core.getInput("repo-token", { required: true });
-    // List of jobs to collect logs from (all jobs when empty)
-    const jobNames = core.getInput("job-names", { required: false });
-    const allowList = getCommaSeparatedInput(jobNames);
-    // LogQL Push endpoint
-    const endpoint = core.getInput("endpoint", { required: false });
-    // LogQL endpoints (when endpoint is not present)
-    const addrValue = core.getInput("addresses", { required: false });
-    const addresses = getCommaSeparatedInput(addrValue);
-    // LogQL partition ID
-    const partitionId = core.getInput("partition", { required: false });
-    // logql user
-    const username = core.getInput("username", { required: false });
-    // logql pass
-    const password = core.getInput("password", { required: false });
+        // Github repo token
+        const repoToken = core.getInput("repo-token", {required: true});
+        // List of jobs to collect logs from (all jobs when empty)
+        const jobNames = core.getInput("job-names", {required: false});
+        const allowList = getCommaSeparatedInput(jobNames);
+        // LogQL Push endpoint
+        const endpoint = core.getInput("endpoint", {required: false});
+        // LogQL endpoints (when endpoint is not present)
+        const addrValue = core.getInput("addresses", {required: false});
+        const addresses = getCommaSeparatedInput(addrValue);
+        // LogQL partition ID
+        const partitionId = core.getInput("partition", {required: false});
+        // logql user
+        const username = core.getInput("username", {required: false});
+        // logql pass
+        const password = core.getInput("password", {required: false});
 
-    // Ensure either endpoint or addresses are set
-    if (endpoint === "" && addresses.length === 0) {
-      throw new Error(
-        "invalid configuration: please set either endpoint or addresses"
-      );
-    }
-
-    // get an authenticated HTTP client for the GitHub API
-    const client = getClient(repoToken);
-
-    // get all the jobs for the current workflow
-    const workflowId = process.env["GITHUB_RUN_ID"] || "";
-    const repo = process.env["GITHUB_REPOSITORY"] || "";
-    core.debug(`Allow listing ${allowList.length} jobs in repo ${repo}`);
-    const jobs = await fetchJobs(client, repo, workflowId, allowList);
-
-    const onConnectionError = (err) => {
-      core.debug("Error at connecting with logs endpoint\n", err);
-    };
-    const lokiBasicAuth = () => {
-      if (username && password) {
-        return `${username}:${password}`;
-      }
-      return "";
-    };
-
-    const lokiFmt = printf(({ message }) => {
-      if (!message || message.length === 0) {
-        return;
-      }
-      const line = message.match(gh_log_regex);
-      if (!line?.groups?.log) {
-        return message;
-      }
-      const { log } = line?.groups;
-      const xlog = `${log}`;
-      return xlog;
-    });
-
-    const options = (job) => {
-      return {
-        transports: [
-          new LokiTransport({
-            host: endpoint,
-            labels: {
-              job: job?.name,
-              jobId: job?.id,
-              repo,
-              workflowId,
-              type: "github",
-            },
-            batching: false,
-            basicAuth: lokiBasicAuth(),
-            format: lokiFmt,
-            onConnectionError: onConnectionError
-          }),
-        ],
-      };
-    };
-    const logger = (job) => createLogger(options(job));
-
-    // get the logs for each job
-    core.debug(`Getting logs for ${jobs.length} jobs`);
-    for (const j of jobs) {
-      const logs = logger(j);
-      const lines = await fetchLogs(client, repo, j);
-      core.debug(`Fetched ${lines.length} lines for job ${j.name}`);
-      for (const l of lines) {
-        try {
-          if (l && l?.length > 0) {
-            logs.info(l);
-          }
-        } catch (e) {
-          core.warning(`Parser error ${e}`);
-          logs.warning(`Error: ${e}`);
+        // Ensure either endpoint or addresses are set
+        if (endpoint === "" && addresses.length === 0) {
+            throw new Error(
+                "invalid configuration: please set either endpoint or addresses"
+            );
         }
-      }
-      logs.clear();
+
+        // get an authenticated HTTP client for the GitHub API
+        const client = getClient(repoToken);
+
+        // get all the jobs for the current workflow
+        const workflowId = process.env["GITHUB_RUN_ID"] || "";
+        const repo = process.env["GITHUB_REPOSITORY"] || "";
+        core.debug(`Allow listing ${allowList.length} jobs in repo ${repo}`);
+        const jobs = await fetchJobs(client, repo, workflowId, allowList);
+
+        const onConnectionError = (err) => {
+            core.debug("Error at connecting with logs endpoint\n", err);
+        };
+        const lokiBasicAuth = () => {
+            if (username && password) {
+                return `${username}:${password}`;
+            }
+            return "";
+        };
+
+        const lokiFmt = printf(({message}) => {
+            if (!message || message.length === 0) {
+                return;
+            }
+            const line = message.match(gh_log_regex);
+            if (!line?.groups?.log) {
+                return message;
+            }
+            const {log} = line?.groups;
+            const xlog = `${log}`;
+            return xlog;
+        });
+
+        const options = (job) => {
+            return {
+                transports: [
+                    new LokiTransport({
+                        host: endpoint,
+                        labels: {
+                            job: job?.name,
+                            jobId: job?.id,
+                            repo,
+                            workflowId,
+                            type: "github",
+                        },
+                        batching: false,
+                        basicAuth: lokiBasicAuth(),
+                        format: lokiFmt,
+                        onConnectionError: onConnectionError
+                    }),
+                ],
+            };
+        };
+        const logger = (job) => createLogger(options(job));
+
+        // get the logs for each job
+        core.debug(`Getting logs for ${jobs.length} jobs`);
+        for (const j of jobs) {
+            const logs = logger(j);
+            const lines = await fetchLogs(client, repo, j);
+            core.debug(`Fetched ${lines.length} lines for job ${j.name}`);
+            for (const l of lines) {
+                try {
+                    if (l && l?.length > 0) {
+                        logs.info(l);
+                    }
+                } catch (e) {
+                    core.warning(`Parser error ${e}`);
+                    logs.warning(`Error: ${e}`);
+                }
+            }
+            logs.clear();
+        }
+    } catch (e) {
+        core.setFailed(`Run failed: ${e}`);
     }
-  } catch (e) {
-    core.setFailed(`Run failed: ${e}`);
-  }
 }
 
 await run();
